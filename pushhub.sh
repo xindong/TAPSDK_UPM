78b53eaa2d98b17edc29948ff8f30188d8b5ad23
xiaoyi <xiaoyi@xd.com>
2021-01-20 14:03:39 +0800

    feat: upm ci script

--- pushhub.sh
+++ pushhub.sh
@@ -1,33 +1,37 @@
-
 #!/bin/sh
 remoteUPM=0
-for repo in $(git remote -v)
-do
-  if [ "$repo"="upm" ];then
-    remoteUPM=1;
-  fi
-done;
 
-hasGit=`which git` #判断是否已安装git
-if [ ! $hasGit ];then
-  echo 'Please download git first!';
-  exit 1;
-else 
+checkBranchExist() {
+  for repo in $(git remote -v); do
+    if [ "$repo"="upm" ]; then
+      remoteUPM=1
+    fi
+  done
+}
+
+hasGit=$(which git) #判断是否已安装git
+if [ ! $hasGit ]; then
+  echo 'Please download git first!'
+  exit 1
+else
   # 获取当前分支
-  branch=`git branch | grep "*"`
+  branch=$(git branch | grep "*")
   # 截取分支名
   currBranch=${branch:2}
   echo $currBranch
   git subtree split --prefix=TDS --branch upm
+
   git checkout upm
-  git tag $1
-  if [ $remoteUPM=1 ];then
+
+  checkBranchExist
+
+  if [ $remoteUPM=1 ]; then
+    echo "Github remove upm repo"
     git remote rm upm
-    echo "Github remove UPM repo"
   fi
   git remote add upm git@github.com:xindong/TAPSDK_UPM.git
+  git fetch upm
   echo "Github add upm repo"
   git push upm upm --tags
-  git checkout $currBranch --force 
+  git checkout $currBranch --force
 fi
-
